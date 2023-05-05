7c058a2d3bd595255651cce24a905e693f016fa7
kiran <kiran.sk1985@gmail.com>
2023-05-05 18:05:13 +0530

    my first commit

---
+++ src/test/java/kiranlearningacademy/Mavenjava/seleniumprac.java
@@ -0,0 +1,16 @@
+package kiranlearningacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class seleniumprac {
+	
+   @Test	
+   public void BrowserAutomation() {
+	   System.out.println("Hi i am from BrowserAutomation");
+   }
+   @Test
+   public void PageAutomation() {
+	   System.out.println("Hi i am from PageAutomation");
+   }
+
+}
